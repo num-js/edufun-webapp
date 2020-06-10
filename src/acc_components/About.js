c9e44830453a8ccea89e0607813d98a969706aff
mdnmnahmed <mdnmnahmed@gmail.com>
2020-06-10 11:25:17 +0530

    Fixing Bugs

--- src/acc_components/About.js
+++ src/acc_components/About.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import SideBar from './SideBar';
-import DashboardFooter from './DashboardFooter';
 import './About.css';
 export default function DashboardHome() {
     return (
